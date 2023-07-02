70f8904f10470800fa15f5e8ef4e6973cbae6292
Yasiru Chamuditha <109359904+yasiruchamuditha@users.noreply.github.com>
2023-07-02 12:32:06 +0530

    Update C_Donor_Login.java

--- BloodBankManagementSystem/src/Controller/C_Donor_Login.java
+++ BloodBankManagementSystem/src/Controller/C_Donor_Login.java
@@ -5,7 +5,11 @@
 package Controller;
 
 import Model.*;
-
+/**
+ *
+ * @author yasiru
+ * contact me : https://linktr.ee/yasiruchamuditha for more information.
+ */
 public class C_Donor_Login 
 {
     public static boolean DonorLogin(String NIC,String Password)
